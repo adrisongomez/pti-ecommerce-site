ec599adb6fd18fb03a22b5ab97fa96d10396e0ee
Adrison Gomez <agomez@resonance.nyc>
2025-03-24 19:37:28 -0400

    chore(infra): add initial infrastructure setup
    Introduce initial infrastructure files including a CDK stack, jest test setup,
    and configuration for npm and pnpm. Also update docker-compose and related
    project settings to support the new infra structure.

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const defaultBucket = new cdk.aws_s3.Bucket(this, "ecommerce-public", {
+      bucketName: "ecommerce-public",
+      encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+      accessControl: cdk.aws_s3.BucketAccessControl.PUBLIC_READ,
+      enforceSSL: true,
+    });
+    new cdk.aws_cloudfront.Distribution(this, "ecommerce-distribution", {
+      defaultBehavior: {
+        origin:
+          cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+            defaultBucket,
+          ),
+      },
+    });
+
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
